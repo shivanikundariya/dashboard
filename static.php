3fae45e7cdc19a2a7862a897ee814652ad549aee
shivanikundariya <shivanikundariya@gmail.com>
2024-09-12 09:15:22 +0530

    Add files via upload

---
+++ static.php
@@ -0,0 +1,9 @@
+<html>
+    <form action="dynamic.php"  method="POST">
+        Enter Name<input type="text"name="about"></br>
+        Enter diparment Name<input type="text"name="nd"></br>
+        Enter college Name<input type="text"name="cn"></br>
+        <input type="submit">
+
+    </form>
+</html>
